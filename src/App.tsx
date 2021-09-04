cc297d8d7fe33fc6eae0aaf297cd391de8371f4f
Dhemes Mota <dhemes.mota@gmail.com>
2021-09-04 10:36:26 -0300

    frist commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import { ApolloProvider } from '@apollo/client';
-
-import { Home } from './pages/Home';
-import { client } from './services/apolloClient';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useMemo } from 'react';
 
 function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: !prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode]
+  );
+
   return (
-    <ApolloProvider client={client}>
-      <div className="App">
-        <Home />
-      </div>
-    </ApolloProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <h1>Hello world</h1>
+    </ThemeProvider>
   );
 }
 
